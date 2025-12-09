c98b8efbd45a6f3892236a2cd73dea62c33ebca1
Antlex <AlexAntlex@yandex.ru>
2025-12-09 14:27:39 +0300

    build: добавлен poetry для контроля зависимостей, а так же README.md

--- app/domain/users/use_cases.py
+++ app/domain/users/use_cases.py
@@ -1,4 +1,3 @@
-from typing import List
 from .repositories import IUserRepository
 from .entities import User
 
--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
+import uvicorn
 from fastapi import FastAPI
 from app.core.database import Base, engine
 from app.presentation.api import users
 
-app = FastAPI(title="Projects Platform")
+app = FastAPI(title="World Pages Creator")
 
 # Создаём таблицы
 Base.metadata.create_all(bind=engine)
 
 # Роуты
 app.include_router(users.router)
+
+@app.get("/")
+def index():
+    return {"projects": {"progect_1", "progect_2", "progect_3", "progect_4"
+                         }
+            }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
